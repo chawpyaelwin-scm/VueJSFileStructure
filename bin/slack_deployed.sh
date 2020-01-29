b28da7b454bddcf25d85d513568af136f549d2ff
chawpyaelwin-scm <chaw.pyaelwin@seattleconsultingmyanmar.com>
2020-01-29 13:09:21 +0630

    deploysh

--- bin/slack_deployed.sh
+++ bin/slack_deployed.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
-# デプロイされたURLをSlackに通知するスクリプト
-# 使い方は
-# $ ./slack_deployed.sh https://example.com
-# のように、通知したいURLを後ろにつけて呼び出す
-
-# 引数を変数に格納
-DEPLOYED_URL=${1:-not_set}
-
-set +xe # デバッグ出力をオフにする
-echo -e "\n\n"
-printf '🎉\e[34m Success! \e[m🎉\n'
-printf '\e[34m The deployed URL: \e[m '${DEPLOYED_URL}"\n"
 
 # デプロイ結果をSlackに通知
 curl -X POST -H 'Content-type: application/json' \
